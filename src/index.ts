5df91f0bd17d6c9d38425a338acea99d6213b977
andres2202 <andresfelipenf@gmail.com>
2025-01-13 16:49:10 -0500

    Enable cors and headers

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,23 @@ import router from "./routes/index.routes";
 import dotenv from 'dotenv';
 import bodyParser from "body-parser";
 import swaggerDocs from "./helpers/SwaggerDocs";
+import cors from "cors";
 
 dotenv.config();
 
 function main(): void {
     const app = express();
     const port = process.env.PORT || 3000;
-
+    const allowedOrigins = [process.env.FRONTEND_URL as string, process.env.FRONTEND_URL_LOCAL as string];
     app.use(express.json());
     app.use(bodyParser.json());
+    app.use(cors({
+        origin: allowedOrigins,
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        exposedHeaders: ['Authorization'],
+      })
+    );
 
     sequelize.sync().then(() => {
         console.log('Database connected');
